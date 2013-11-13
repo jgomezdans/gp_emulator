42a16303f1459b6653d108f19b33918f908461ed
José Gómez-Dans <jgomezdans@gmail.com>
2013-11-13 16:36:00 +0000

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gp_emulator',
+      version='1.0',
+      description='A Python GaussianProcess emulator software package',
+      author='J Gomez-Dans',
+      author_email='j.gomez-dans@ucl.ac.uk',
+      url='http://bitbucket.org/gomezdansj/gp_emulator',
+      packages=['gp_emulator'],
+     )
