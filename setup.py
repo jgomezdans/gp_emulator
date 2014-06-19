95eb71267e568cd56a151888c9336afbd64ca30e
J Gomez-Dans <j.gomez-dans@ucl.ac.uk>
2014-06-19 18:55:40 +0100

    Bumped verstion up

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='gp_emulator',
-      version='1.3.1',
+      version='1.3.2',
       description='A Python GaussianProcess emulator software package',
       author='J Gomez-Dans',
       author_email='j.gomez-dans@ucl.ac.uk',
