7c3a20221ada5fbb022788f1f21165acd9086c74
José Gómez-Dans <jgomezdans@gmail.com>
2015-10-30 13:07:13 +0000

    Make sure the new function to convert emulators  is exported to the package namespace

--- gp_emulator/__init__.py
+++ gp_emulator/__init__.py
@@ -2,5 +2,5 @@
 from GaussianProcess import GaussianProcess, k_fold_cross_validation
 from multivariate_gp import MultivariateEmulator
 from lhd import lhd
-from save_emulators import EmulatorStorage
+from save_emulators import EmulatorStorage, convert_npz_to_hdf5
 from emulation_helpers import create_training_set, create_validation_set
