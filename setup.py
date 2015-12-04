47e27c6807fccd5be7b0e15176c2bc47a26ffbb7
José Gómez-Dans <jgomezdans@gmail.com>
2015-12-04 17:20:23 +0000

    Added a better description of the software

--- setup.py
+++ setup.py
@@ -5,8 +5,19 @@ from distutils.core import setup
 setup(name='gp_emulator',
       version='1.5',
       description='A Python GaussianProcess emulator software package',
+      classifiers=[
+	'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Environment :: Console']
       author='J Gomez-Dans',
       author_email='j.gomez-dans@ucl.ac.uk',
-      url='http://bitbucket.org/gomezdansj/gp_emulator',
+      url='http://github.com/jgomezdans/gp_emulator',
       packages=['gp_emulator'],
      )
