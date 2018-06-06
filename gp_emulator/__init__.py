c0fa633f6e186335a8247567d5d86fc73773ef84
Jose Gomez-Dans <jgomezdans@gmail.com>
2018-06-06 15:28:38 +0100

    Removed staled emulator storage call

--- gp_emulator/__init__.py
+++ gp_emulator/__init__.py
@@ -2,7 +2,6 @@
 from .GaussianProcess import GaussianProcess, k_fold_cross_validation
 from .multivariate_gp import MultivariateEmulator
 from .lhd import lhd
-from .save_emulators import EmulatorStorage, convert_npz_to_hdf5
 from .emulation_helpers import create_training_set, create_validation_set
 from .emulation_helpers import create_emulator_validation
 from .emulation_helpers import create_single_band_emulators
