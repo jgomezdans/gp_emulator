90e6db776963bd78ea00515ecbc5080f66b0466b
José Gómez-Dans <jgomezdans@gmail.com>
2018-09-20 16:18:45 +0100

    Seems to work on Python 2 **and** 3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), "rb") as f:
-    long_description = f.read().decode()
+    long_description = f.read().decode('utf-8')
 
 setup(name='gp_emulator',
       version='1.6.3',
@@ -27,5 +27,5 @@ setup(name='gp_emulator',
       author_email='j.gomez-dans@ucl.ac.uk',
       url='http://github.com/jgomezdans/gp_emulator',
       packages=['gp_emulator'],
-      install_requires=["numpy>=1.13", "scipy>=1.0"]
+      install_requires=["numpy", "scipy"]
      )
