d776820115259f2f7f39afd8c5ac2d924108489f
Jose Gomez-Dans <jgomezdans@gmail.com>
2018-10-28 20:06:32 +0000

    Updated configuration for RTD

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 #sys.path.append(os.path.abspath('sphinxext'))
+
+sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 import matplotlib
 # -- Project information -----------------------------------------------------
 
